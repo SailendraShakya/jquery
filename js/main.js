f89f07d7907031c525edb385c48100c07d5927ce
Sailendra Shakya <sailendrashakya@Sailendras-MacBook-Pro.local>
2017-08-07 20:04:08 +0545

    added new function in click event

--- js/main.js
+++ js/main.js
@@ -1,9 +1,32 @@
 $(document).ready(function(){
+	var toggle = true;
+	var count = 0;
+	var colorlist = ['#e6f7ff','#ffffff','#ffb3ff','#66ff66'];
 	$("p").click(function(){
 		$(this).hide();
 	});
 
+	$("a.button").click(function(){
+
+		if(toggle){
+			$(this).parent('.city').css('background-color',' #e6f7ff');
+			toggle = false;
+		}else{
+			$(this).parent('.city').css('background-color',' #ffffff');
+			toggle = true;
+		}
+		
+	});
+
 	$("#myButton").click(function(){
-		alert("Don't hit me");
+		if(count != colorlist.length){
+			$(this).parent('.city').css('background-color', colorlist[count]);
+			count++;
+		}else{
+			count = 0;
+			$(this).parent('.city').css('background-color', colorlist[count]);
+			
+		}
+		
 	});
 });
