8f5d9e367a5b778e1b71a28332d9d7210cb48f0d
shwetasth <shweta.sth@gmail.com>
2017-08-09 22:39:28 +0545

    Modified index.html and main.js

--- js/main.js
+++ js/main.js
@@ -1,32 +1,69 @@
 $(document).ready(function(){
-	var toggle = true;
+	var toggle = true; 
 	var count = 0;
+	var myVar;
 	var colorlist = ['#e6f7ff','#ffffff','#ffb3ff','#66ff66'];
 	$("p").click(function(){
 		$(this).hide();
 	});
 
-	$("a.button").click(function(event){
-		event.preventDefault();
-		if(toggle){
+	$(".stopButton").click(function(){
+		
+		var selector = $(this);
+		console.log(selector.attr('data-status'));
+		if(selector.data('status') ==1){
+			console.log("Shweta");
+		}
+		else{
+			console.log("Sailendra");
+		}
+		//event.preventDefault();
+		stopFunction();
+		/*if(toggle){
 			$(this).parent('.city').css('background-color',' #e6f7ff');
 			toggle = false;
 		}else{
 			$(this).parent('.city').css('background-color',' #ffffff');
 			toggle = true;
 		}
+		*/
+	});
+
+	$(".myButton").click(function(){
+		var count = 0;
+		var selector = $(this);
+		myVar = setInterval(function(){myFunction(selector)},500);
+		/*
+		if(count != colorlist.length){
+			//alert(count);
+		setInterval(function(){	
+			$(this).parent('.city').css('background-color', colorlist[count]);
+			},5000);
+		count++;
+		}else{
+			count = 0;
+			//$(this).parent('.city').css('background-color', colorlist[count]);
+		}
+		*/
 		
 	});
 
-	$("#myButton").click(function(){
+
+
+	function myFunction(selector) {
+		//console.log(count);
+		//console.log(colorlist[count]);		
 		if(count != colorlist.length){
-			$(this).parent('.city').css('background-color', colorlist[count]);
+			selector.parent('.city').css('background-color', colorlist[count]);
 			count++;
 		}else{
 			count = 0;
-			$(this).parent('.city').css('background-color', colorlist[count]);
-			
+			selector.parent('.city').css('background-color', colorlist[count]);
 		}
-		
-	});
+	}
+
+	function stopFunction(){
+		clearInterval(myVar);
+		console.log(colorlist[count]);
+	}
 });
